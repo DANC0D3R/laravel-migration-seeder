1f93a37793c3c0dd168be509b68b1c75687a4b7a
Daniele Minieri <danieleminieri99@gmail.com>
2023-03-14 10:43:12 +0100

    Milestone 1 completata

---
+++ database/migrations/2023_03_14_085557_create_trains_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            // - Azienda - TIPO: Stringa
+            $table->string('company', 64);
+            // - Stazione di partenza - TIPO: STRINGA
+            $table->string('departure_station', 128);
+            // - Stazione di arrivo - TIPO: STRINGA
+            $table->string('arrival_station', 128);
+            // - Orario di partenza - TIPO: DATETIME / TIMESTAMP / TIME (da aggiungere la colonna di tipo DATE)
+            $table->dateTime('departure_time');
+            // - Orario di arrivo - TIPO: DATETIME / TIMESTAMP / TIME (da aggiungere la colonna di tipo DATE)
+            $table->dateTime('arrival_time');
+            // - Codice Treno - TIPO: STRINGA
+            $table->string('code', 16);
+            // - Numero Carrozze - TIPO: TINYINT UNSIGNED
+            $table->unsignedTinyInteger('carriages');
+            // - In orario - TIPO: TINYINT (BOOLEAN)
+            $table->boolean('on_time')->default(true);
+            // - Cancellato - TIPO: TINYINT
+            $table->boolean('canceled')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
